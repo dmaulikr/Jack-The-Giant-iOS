61d9b52d1c8e4792dc2964eac4c78176f8c617f9
Ken <kenaramaki@gmail.com>
2017-06-22 09:08:09 -0300

    player animations

--- GameplayScene.swift
+++ GameplayScene.swift
@@ -20,7 +20,9 @@ class GameplayScene: SKScene {
     override func didMove(to view: SKView) {
         center = (self.scene?.size.width)! / (self.scene?.size.height)!
         
-        player = self.childNode(withName: "Player") as? Player
+        player = self.childNode(withName: "Player") as? Player//!
+        player?.initializePlayerAndAnimations()
+        
     
     }
     
@@ -37,8 +39,10 @@ class GameplayScene: SKScene {
             
             if location.x > center! {
                 moveLeft = false
+                player?.animatePlayer(moveLeft: moveLeft)
             } else {
                 moveLeft = true
+                player?.animatePlayer(moveLeft: moveLeft)
             }
         }
         
@@ -48,6 +52,7 @@ class GameplayScene: SKScene {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         canMove = false
+        player?.stopPlayerAnimation()
     }
 
     
--- Player.swift
+++ Player.swift
@@ -10,6 +10,43 @@ import SpriteKit
 
 class Player: SKSpriteNode {
     
+    private var texttureAtlas = SKTextureAtlas()
+    private var playerAnimation = [SKTexture]()
+    private var animatePlayerAction = SKAction()
+    
+    
+    func initializePlayerAndAnimations() {
+        
+        texttureAtlas = SKTextureAtlas(named: "Player.atlas")
+        
+        for i in 2...texttureAtlas.textureNames.count {
+            let name = "Player \(i)"
+            playerAnimation.append(SKTexture(imageNamed: name))
+            
+        }
+        
+        animatePlayerAction = SKAction.animate(with: playerAnimation, timePerFrame: 0.08, resize: true, restore: false)
+    }
+    
+    
+    func animatePlayer(moveLeft: Bool) {
+        
+        if moveLeft {
+            self.xScale = -fabs(self.xScale)
+        } else {
+            self.xScale = fabs(self.xScale)
+        }
+        
+        self.run(SKAction.repeatForever(animatePlayerAction), withKey: "Animate")
+    }
+    
+    func stopPlayerAnimation(){
+        self.removeAction(forKey: "Animate")
+        self.texture = SKTexture(imageNamed: "Player 1")
+        self.size = (self.texture?.size())!
+    }
+    
+    
     func movePlayer(moveLeft: Bool) {
         
         if moveLeft {
